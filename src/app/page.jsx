6d49a73a53f805f7df1b33b711b1f069205dda9d
bjmczhang <bjmczhang@gmail.com>
2024-06-15 19:24:54 +1000

    first commit

---
+++ src/app/page.jsx
@@ -0,0 +1,48 @@
+import Link from "next/link";
+
+
+export default function Home() {
+    return <div>
+        <Link href="/">
+            <h1>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Architecto beatae doloremque exercitationem
+                facere ipsam laboriosam nobis optio rerum tempora voluptatibus?</h1>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium adipisci consectetur,
+            cumque dolore eveniet ipsam molestias mollitia, nam pariatur quasi, qui sapiente sequi sint sit tempora. At
+            doloribus eveniet inventore ipsum labore pariatur velit! Animi architecto autem consectetur, consequatur
+            culpa dignissimos dolorem ducimus eius eos excepturi fuga fugit harum impedit laborum nesciunt nostrum,
+            possimus provident quas rem repellendus reprehenderit rerum tempora? Aperiam aspernatur cum cupiditate
+            impedit iure nam neque quasi tempora velit vitae? Blanditiis, eligendi eum eveniet excepturi explicabo fugit
+            incidunt modi non reiciendis repudiandae sequi, sit veritatis? Adipisci aliquid animi corporis dicta error
+            id illum impedit ipsum, laudantium magnam perspiciatis quae quibusdam recusandae sapiente sequi sint soluta
+            totam vel voluptas voluptatum? Amet architecto corporis, deserunt distinctio earum esse expedita, facilis
+            illo ipsa iste laudantium minima modi molestiae nesciunt nobis officiis quae quasi ratione repellendus,
+            reprehenderit temporibus velit voluptatibus. Dolores eos, esse numquam porro quaerat repudiandae?
+            Consectetur consequuntur dicta dolore enim et facere itaque laudantium libero natus, nesciunt nostrum omnis
+            optio placeat praesentium, quam quas reprehenderit saepe totam ut voluptatem. Accusantium, aliquid amet
+            architecto blanditiis delectus dignissimos dolores ducimus ea excepturi exercitationem expedita fugit hic
+            impedit itaque neque nisi, nostrum obcaecati odio quas quo ratione recusandae repellendus, saepe temporibus
+            totam ullam unde veritatis. Et numquam reiciendis vero. A amet atque consectetur consequatur cupiditate eius
+            esse eveniet exercitationem illum molestiae molestias, optio quia quibusdam reprehenderit rerum soluta sunt
+            vero voluptatum. Ab deleniti doloribus expedita facere id iure quis rerum sapiente vero, voluptatum. Aliquid
+            blanditiis corporis, deserunt doloribus expedita nostrum officiis porro repellendus sint soluta tempora
+            veritatis voluptatem, voluptatibus. Alias, at, quibusdam! Facilis fugit hic minima repellendus temporibus!
+            Adipisci amet animi assumenda aut cum delectus deserunt dolor doloremque enim eveniet excepturi expedita
+            fuga, fugiat ipsam iusto molestias neque odit officiis, optio pariatur perferendis provident quaerat,
+            quisquam reiciendis repudiandae sequi sunt temporibus tenetur vitae voluptatibus. A ad adipisci alias
+            asperiores atque consequatur corporis debitis dignissimos dolor dolore ducimus esse fugiat harum iure,
+            laudantium nam nisi odio officia praesentium quae quam quasi reprehenderit soluta sunt tenetur voluptas
+            voluptatibus! Beatae commodi dicta ducimus quibusdam quisquam repellat similique unde voluptate. Ad
+            blanditiis cumque dicta dolore dolorem fugiat inventore ipsum minima, modi nostrum optio placeat possimus,
+            quas quia quidem ratione rem tenetur vitae voluptas voluptatibus. A ad blanditiis distinctio doloribus
+            expedita magnam neque, nostrum perferendis possimus provident repellat tempora ullam? Maxime non quae
+            voluptatem! Ab eaque iste nam omnis, quae reiciendis repellat. Eligendi eum illum iste magnam nesciunt!
+            Aliquam animi, aperiam at blanditiis consectetur distinctio dolore earum eligendi explicabo maxime minima,
+            nihil placeat provident quaerat quisquam recusandae rem repudiandae sint sunt voluptatum. Alias aperiam
+            architecto atque consectetur, consequuntur deserunt eum excepturi expedita facere impedit ipsa ipsam iusto
+            labore libero magni officia officiis optio placeat, quidem, recusandae repellat repellendus repudiandae
+            temporibus vero voluptatem! Ab ad aperiam assumenda at autem consectetur culpa cum dicta dolorem doloremque
+            ea et illo itaque magni maiores natus neque obcaecati omnis quaerat, qui quidem quo quod ratione repellat
+            repudiandae totam voluptatibus. Autem deserunt dicta et odio officia pariatur quas. Blanditiis maiores quod
+            sed? Deserunt!</Link>
+    </div>;
+}
